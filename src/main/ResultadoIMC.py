89a09c3ff99ec70177d9faa9b96125e787aaeab1
wilicarol <wili.carol0235@gmail.com>
2024-05-06 21:20:15 -0300

    :bulb: docs: Criando classe ResultadoIMC

---
+++ src/main/ResultadoIMC.py
@@ -0,0 +1,5 @@
+class ResultadoIMC:
+    def __init__(self, estado: str, criticidade: int, comentario: str):
+        self.estado = estado
+        self.criticidade = criticidade
+        self.comentario = comentario
