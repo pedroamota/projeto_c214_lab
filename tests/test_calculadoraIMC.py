3848dfd2d77a8f04cb2f42d85ea65a6839c5d5c6
lucerothayana <thayana.xavier@geb.inatel.br>
2024-05-23 21:48:43 -0300

    Create test_calculadoraIMC.py

---
+++ tests/test_calculadoraIMC.py
@@ -0,0 +1,26 @@
+import pytest
+from src.CalculadoraIMC import CalculadoraIMC, AlturaNegativa, PesoNegativo, IMCService
+
+def test_calcularIMC():
+    imc_service = IMCService()
+    calculadora = CalculadoraIMC(imc_service)
+
+    altura = 1.75
+    peso = 70
+    imc = calculadora.calcular_imc(altura, peso)
+    
+    assert imc == pytest.approx(22.86, 0.01)
+
+def test_pesoNegativo():
+    imc_service = IMCService()
+    calculadora = CalculadoraIMC(imc_service)
+    
+    with pytest.raises(PesoNegativo):
+        calculadora.calcular_imc(1.75, -70)
+
+def test_alturaNegativa():
+    imc_service = IMCService()
+    calculadora = CalculadoraIMC(imc_service)
+    
+    with pytest.raises(AlturaNegativa):
+        calculadora.calcular_imc(-1.75, 70)
